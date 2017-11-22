37a6980d7b2dde2da97f376c2834cfb57387caf2
Romain TRUCHI <romain.truchi.06@gmail.com>
2017-11-22 18:33:34 +0100

    Cube.move()

--- Cube.js
+++ Cube.js
@@ -14,10 +14,12 @@ class Cube extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { x: 0, y: 0, z: 0 }
+    this.layers = this.getLayers()
+    this.state  = { x: 0, y: 0, z: 0 }
+    this.move   = this.move.bind(this)
   }
 
-  layers() {
+  getLayers() {
     let layers = new Map
 
     React.Children.forEach(this.props.children, Face => {
@@ -34,13 +36,104 @@ class Cube extends Component {
     return Array.from(layers, ([key, val]) => [key, val])
   }
 
+  move(direction) {
+    let coords = this.state
+
+    let faces, list
+    switch (direction.toUpperCase()) {
+      case 'UP':
+        faces    = this.getCol(coords)
+        list     = faces.map(Face => +Face.props.y)
+        coords.y = +faces[
+          this._mod(list.indexOf(+coords.y) - 1, list.length)
+        ].props.y
+        break;
+      case 'DOWN':
+        faces    = this.getCol(coords)
+        list     = faces.map(Face => +Face.props.y)
+        coords.y = +faces[
+          this._mod(list.indexOf(+coords.y) + 1, list.length)
+        ].props.y
+        break;
+      case 'LEFT':
+        faces    = this.getRow(coords)
+        list     = faces.map(Face => +Face.props.x)
+        coords.x = +faces[
+          this._mod(list.indexOf(+coords.x) - 1, list.length)
+        ].props.x
+        break;
+      case 'RIGHT':
+        faces    = this.getRow(coords)
+        list     = faces.map(Face => +Face.props.x)
+        coords.x = +faces[
+          this._mod(list.indexOf(+coords.x) + 1, list.length)
+        ].props.x
+        break;
+      case 'IN':
+        faces    = this.getAisle(coords)
+        list     = faces.map(Face => +Face.props.z)
+        coords.z = +faces[
+          this._mod(list.indexOf(+coords.z) - 1, list.length)
+        ].props.z
+        break;
+      case 'OUT':
+        faces    = this.getAisle(coords)
+        list     = faces.map(Face => +Face.props.z)
+        coords.z = +faces[
+          this._mod(list.indexOf(+coords.z) + 1, list.length)
+        ].props.z
+        break;
+    }
+
+    this.setState(coords)
+  }
+
+  getAisle({ x, y }) {
+    let a = this.layers.map(layer => layer[1].filter(Face => +Face.props.x === +x && +Face.props.y === +y))
+    let b = Array.prototype.concat.apply([], a)
+  }
+
+  getRow({ y, z }) {
+    return this._getRow(this._getLayer(z), { y })
+  }
+
+  getCol({ x, z }) {
+    return this._getRow(this._getLayer(z), { x })
+  }
+
+  _getLayer(z) {
+    return this.layers.filter(layer => +layer[0] === +z)[0][1]
+  }
+
+  _getRow(layer, { x, y }) {
+    let dir, test
+    const filter = Face => +Face.props[dir] === +test
+
+    if (typeof x !== 'undefined') {
+      [dir, test] = ['x', x]
+    } else if (typeof y !== 'undefined') {
+      [dir, test] = ['y', y]
+    } else {
+      return []
+    }
+
+    let opp = dir === 'x' ? 'y' : 'x'
+    return layer
+      .filter(filter)
+      .sort((Face1, Face2) => Face1.props[opp] - Face2.props[opp])
+  }
+
+  _mod(x, n) {
+    return ((x % n) + n) % n
+  }
+
   render() {
     return (
       <Cube$>
-        {this.layers().map((layer, i) =>
+        {this.layers.map((layer, i) =>
           <Layer key={i} z={layer[0]} coords={this.state}>
             {layer[1].map((Face, j) =>
-              React.cloneElement(Face, { key: j })
+              React.cloneElement(Face, { key: j, move: this.move })
             )}
           </Layer>
         )}
